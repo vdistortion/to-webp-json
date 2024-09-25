d001262d142287d4ecbe7ebab57466e713e23b05
Валентин <valentin.distortion@gmail.com>
2024-09-25 08:50:44 +0500

    refactor directory-structure-json

--- src/modules/to-json.ts
+++ src/modules/to-json.ts
@@ -5,37 +5,41 @@ import { getPath } from './get-path.js';
 
 export const toJson = (jsonName: string, basePath: string) =>
   new Promise((resolve, reject) => {
-    getStructure(fs, basePath, (error: Error, structure: Folder) => {
-      if (error) reject(error);
+    getStructure(
+      fs,
+      basePath,
+      (error: Error | null, structure: Folder | Array<File | Folder> | undefined) => {
+        if (error) reject(error);
 
-      const list: string[] = [];
-      const imageFilter = (_key: string, value: Folder) => {
-        if (Array.isArray(value)) {
-          return value.filter((item) => {
-            const isFolder = item.type === 'folder';
-            const isImageFile = item.type === 'file' && isImage(item.name);
-            return isFolder || isImageFile;
-          });
+        const list: string[] = [];
+        const imageFilter = (_: string, value: Folder | Array<File | Folder>) => {
+          if (Array.isArray(value)) {
+            return value.filter((item) => {
+              const isFolder = item.type === 'folder';
+              const isImageFile = item.type === 'file' && isImage(item.name);
+              return isFolder || isImageFile;
+            });
+          }
+          return value;
+        };
+        const json = JSON.stringify(structure, imageFilter, 2);
+
+        if (Array.isArray(structure)) {
+          traverseStructure(
+            structure,
+            '.',
+            () => {},
+            (file: File, path: string) => {
+              const fullPath = getPath(path, file.name);
+              list.push(fullPath);
+            },
+          );
         }
-        return value;
-      };
-      const json = JSON.stringify(structure, imageFilter, 2);
 
-      if (structure) {
-        traverseStructure(
-          structure,
-          '.',
-          () => {},
-          (file: File, path: string) => {
-            const fullPath = getPath(path, file.name);
-            list.push(fullPath);
-          },
-        );
-      }
-
-      fs.writeFile(jsonName, json, (err) => {
-        if (err) reject(err);
-        resolve(list);
-      });
-    });
+        fs.writeFile(jsonName, json, (err) => {
+          if (err) reject(err);
+          resolve(list);
+        });
+      },
+    );
   });
